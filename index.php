9d7cf591815f8e88889b7de2dcdd3c618ea2e4bb
abhiram395 <131740961+abhiram395@users.noreply.github.com>
2024-08-29 13:31:44 +0530

    added

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php 
-    $server="localhost";
-    $username="root";
-    $password= "";
-    $con=mysqli_connect($server,$username,$password);
-    if(!$con){
-        die("connection to this database failed".mysqli_connect_error());
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "signin"; // Replace with your actual database name
+
+    // Create connection
+    $con = mysqli_connect($server, $username, $password, $database);
+
+    // Check connection
+    if (!$con) {
+        die("Connection to this database failed: " . mysqli_connect_error());
     }
-    else{
-    echo"connection successfull";
-    }
+    echo "Connection successful";
 ?>
